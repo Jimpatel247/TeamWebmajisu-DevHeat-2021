2556bf37231c8345bcc9ae112d6a52cd4433a09a
Jimpatel247 <85627575+Jimpatel247@users.noreply.github.com>
2021-11-13 20:40:14 +0530

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 
 import './App.css';
+//import About from './components/About';
+import Navbar from './components/Navbar';
+
+import React, { useState } from 'react';
+
+
 
 function App() {
+    const [mode,setMode] = useState('light');
+
+    const toggleMode=()=>{
+      if(mode==='light')
+      {
+        setMode('dark');
+        document.body.style.background='#0c403d';
+      }
+      else{
+        setMode('light');
+        document.body.style.background='white';
+      }
+    }
   return (
-    <div className="App">
-      
-        
-      
-    </div>
+  <>
+   
+<Navbar title ="Study Easy"  aboutus="About us " mode={mode} toggleMode= {toggleMode}/>
+<div className="container  my-2" >
+
+ { /*<About/> */}
+</div>
+  </>
   );
 }
 
