ce7e2aa1fd3b7b312fab5bde781fb3eefcb4cb89
07manan <07manan1850@gmail.com>
2021-11-14 12:43:39 +0530

    footer and home design

--- src/App.js
+++ src/App.js
@@ -8,41 +8,43 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  
+
 } from "react-router-dom";
+import Footer from './components/Footer';
 
 function App() {
-    const [mode,setMode] = useState('light');
+  const [mode, setMode] = useState('light');
 
-    const toggleMode=()=>{
-      if(mode==='light')
-      {
-        setMode('dark');
-        document.body.style.background='#0c403d';
-      }
-      else{
-        setMode('light');
-        document.body.style.background='white';
-      }
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.background = '#0c403d';
     }
+    else {
+      setMode('light');
+      document.body.style.background = 'white';
+    }
+  }
   return (
-  <>
-   <Router>
-<Navbar title ="Study Easy"  aboutus="About us " mode={mode} toggleMode= {toggleMode}/>
-<div className="container  my-2" >
-<Switch>
-          <Route exact path="/about">
-            <About />
-          </Route>
+    <>
+      <Router>
+        <Navbar title="Study Easy" aboutus="About us " mode={mode} toggleMode={toggleMode} />
+        <div className="container  my-2" >
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
           
-          <Route exact path="/">
-            <Home/>
-          </Route>
-        </Switch>
-  
-</div>
-</Router>
-  </>
+
+        </div>
+      </Router>
+      <Footer/>
+    </>
   );
 }
 
